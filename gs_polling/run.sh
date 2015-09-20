33b2923c6635ac5ca9ebe198dd56d54148349de1
Kevin Pan <xcpankevin@hotmail.com>
2015-09-19 21:40:13 -0400

    Adds gsutils script

---
+++ gs_polling/run.sh
@@ -0,0 +1,10 @@
+mkdir -p image_queue
+
+FILES="$(gsutil ls gs://prototypr-images)"
+
+gsutil cp gs://prototypr-images/* image_queue/
+gsutil mv gs://prototypr-images/* gs://prototypr-images-processed/
+
+# now call image processor application
+
+# now call json to react
